d6610b246783b5df6827b162d46e546b0a3b6cd8
J Alfonso Martinez Baeza <mtz.alfonso96@gmail.com>
2020-09-02 00:06:23 -0500

    add float action button and snackbar

---
+++ app/src/main/java/com/alfonso/materialdesign/DetalleMascota.java
@@ -0,0 +1,13 @@
+package com.alfonso.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleMascota extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mascota_detalle);
+    }
+}
--- app/src/main/java/com/alfonso/materialdesign/MainActivity.java
+++ app/src/main/java/com/alfonso/materialdesign/MainActivity.java
@@ -3,6 +3,12 @@ package com.alfonso.materialdesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.aggregateFAB();
+    }
+
+    public void aggregateFAB(){
+        FloatingActionButton miFAB = findViewById(R.id.fabMiFAB);
+
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, getResources().getString(R.string.mensaje), Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.txt_action), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Log.i("SNACKBAR","Click en Snackbar");
+                            }
+                        })
+                        .show();
+            }
+        });
     }
 }
