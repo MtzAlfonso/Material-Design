14e90edbdb5ad51ad7c07d8f8770bd2966c1fa87
J Alfonso Martinez Baeza <mtz.alfonso96@gmail.com>
2020-09-03 00:09:34 -0500

    add SwipeRefreshView

--- app/src/main/java/com/alfonso/materialdesign/MainActivity.java
+++ app/src/main/java/com/alfonso/materialdesign/MainActivity.java
@@ -1,10 +1,14 @@
 package com.alfonso.materialdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,11 +16,34 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    SwipeRefreshLayout srRefresh;
+    ListView lvMyList;
+    Adapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.aggregateFAB();
+        aggregateFAB();
+
+        srRefresh = findViewById(R.id.srRefresh);
+        lvMyList = findViewById(R.id.lvMyList);
+
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lvMyList.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+
+        srRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshContent();
+            }
+        });
+    }
+
+    public void refreshContent(){
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lvMyList.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+        srRefresh.setRefreshing(false);
     }
 
     public void aggregateFAB(){
